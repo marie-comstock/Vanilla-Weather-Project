aceb73ecb4bdf088338d61f2039ccdacde1239cb
marie-comstock <75152398+marie-comstock@users.noreply.github.com>
2021-02-18 22:18:45 -0700

    Added the current temperature Api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+    console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let precipitationElement = document.querySelector("#precipitation");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    console.log(windElement);
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    precipitationElement.innerHTML = response.data.main;
+}
 
+let apiKey = "7d3c997d1246915174451d0269ffae14";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
